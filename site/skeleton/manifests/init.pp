ce90a689371f2b55badfac88b170e9f4f952a0cf
Samuel <samuel@puppetlabs.com>
2016-02-23 10:50:38 +0000

    remove leading space typo

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -2,7 +2,7 @@ class skeleton {
   file { '/etc/skel' :
     ensure => directory,
   }
-  file { ' /etc/skel/.bashrc' :
+  file { '/etc/skel/.bashrc' :
     ensure => present,
     source => "puppet:///modules/skeleton/bashrc"
   }
