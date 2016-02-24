e794760bb79380eef85cda4bdf64fe5ddb0d1e22
Samuel <samuel@puppetlabs.com>
2016-02-24 20:51:19 +0000

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,4 +1,12 @@
 class nginx {
+
+case $operatingsystem {
+  'RedHat', 'CentOS': { include nginx::redhat  } # apply the redhat vars
+  /^(Debian|Ubuntu)$/:{ include nginx::debian  } # apply the debian vars
+  'Windows':          { include nginx::windows  } # apply the windows vars
+  default:            { fail("cannot process ERB variables for ${operatingsystem}")  } #fail otherwise
+}
+
 package { 'nginx':
   ensure => present,
 }
@@ -15,28 +23,21 @@ ensure => directory
 
 file { '/etc/nginx/nginx.conf':
   ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  source  => 'puppet:///modules/nginx/nginx.conf',
+  #source  => 'puppet:///modules/nginx/nginx.conf',
+  content => template('nginx/nginx.conf.erb'),
   require => Package['nginx'],
 }
 
 file { '/etc/nginx/conf.d/default.conf': 
   ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  source  => 'puppet:///modules/nginx/default.conf',
+  #source  => 'puppet:///modules/nginx/default.conf',
+  content => template('nginx/default.conf.erb'),
   require => Package['nginx'],
 }
 
 
 file { '/var/www/index.html': 
   ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
   source  => 'puppet:///modules/nginx/index.html',
   require => Package['nginx'],
 }
