cf156b41343b2b1fb4e40b81687f4e72042c0cd6
Samuel <samuel@puppetlabs.com>
2016-02-24 20:58:08 +0000

    Create windows.pp

---
+++ site/nginx/manifests/windows.pp
@@ -0,0 +1,8 @@
+class nginx::windows {
+doc_root = '/var/www/'
+user = 'nginx'
+log_dir = '/var/log/nginx/'
+pid_dir = '/var/log/nginx/'
+conf_dir = '/etc/nginx/'
+server_dir = '/etc/nginx/conf.d/'
+}
