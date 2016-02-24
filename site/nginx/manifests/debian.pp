16c7eed03a11a6ae1eec8e2c37e8053f7e47c48e
Samuel <samuel@puppetlabs.com>
2016-02-24 20:57:55 +0000

    Create debian.pp

---
+++ site/nginx/manifests/debian.pp
@@ -0,0 +1,8 @@
+class nginx::debian {
+doc_root = '/var/www/'
+user = 'nginx'
+log_dir = '/var/log/nginx/'
+pid_dir = '/var/log/nginx/'
+conf_dir = '/etc/nginx/'
+server_dir = '/etc/nginx/conf.d/'
+}
