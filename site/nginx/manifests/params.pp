7f094cd4f1b84b1e2c3375d8b5edb62d01ea6d0f
Samuel <samuel@puppetlabs.com>
2016-02-24 21:27:11 +0000

    Update params.pp

--- site/nginx/manifests/params.pp
+++ site/nginx/manifests/params.pp
@@ -10,19 +10,19 @@ case $operatingsystem {
   } # apply the redhat vars
   /^(Debian|Ubuntu)$/:{
     $doc_root = '/var/www/'
-    $user = 'nginx'
+    $user = 'www-data'
     $log_dir = '/var/log/nginx/'
     $pid_dir = '/var/log/nginx/'
     $conf_dir = '/etc/nginx/'
     $server_dir = '/etc/nginx/conf.d/'
   } # apply the debian vars
   'Windows':          {
-    $doc_root = '/var/www/'
-    $user = 'nginx'
-    $log_dir = '/var/log/nginx/'
-    $pid_dir = '/var/log/nginx/'
-    $conf_dir = '/etc/nginx/'
-    $server_dir = '/etc/nginx/conf.d/'
+    $doc_root = 'C:/ProgramData/nginx/html/'
+    $user = 'nobody'
+    $log_dir = 'C:/ProgramData/nginx/logs/'
+    $pid_dir = 'C:/ProgramData/nginx/logs/'
+    $conf_dir = 'C:/ProgramData/nginx/'
+    $server_dir = 'C:/ProgramData/nginx/conf.d/'
   } # apply the windows vars
   default:            { fail("cannot process ERB variables for ${operatingsystem}")  } #fail otherwise
 }
